62b3a737a4a4aedebaadc848f0389cc73d1dc5c6
Ratiarisandy Dinoh <dinohratiarisandy@gmail.com>
2023-11-28 19:27:25 +0300

    fix bug (vertical win check)

--- src/index.ts
+++ src/index.ts
@@ -21,10 +21,10 @@ const COL = board_state[0].length;
 // Function to handle making a move when a cell is clicked
 function make_move(e: Event): void {
     // Identify the selected cell
-    const cell_selected = e.target as HTMLElement;
+    const column = e.target as HTMLElement;
 
     // Extract the column from the cell's class
-    const col = cell_selected.classList[1][cell_selected.classList[1].length - 1];
+    const col = column.classList[1];
     const disc_active = document.querySelector(`.disc_${col}`);
 
     // Determine the valid row for the selected column
@@ -43,13 +43,15 @@ function make_move(e: Event): void {
 
         // Check if the current player has won
         if (check_winner(valid_row, parseInt(col))) {
+            info.textContent = `Player ${player === "red" ? 1 : 2} won!`;
+            remove_event();
             return;
         }
 
         // Check if the game is a draw
         if (is_tie()) {
-            remove_event();
             info.textContent = "The game is a draw!";
+            remove_event();
             return;
         }
 
@@ -60,58 +62,22 @@ function make_move(e: Event): void {
     }    
 }
 
+// Function to find the valid row for a given column
+function take_valid_place(col: number): number {
+    for (let r = ROW - 1; r >= 0; r--) {
+        if (board_state[r][col] === 0) {
+            return r;
+        }
+    }
+    return -1;
+}
+
 // Function to check if a player has won the game
 function check_winner(row: number, col: number): boolean {
     if (check_vertical(row, col) || check_horizontal(row, col) || check_diagonals(row, col)) {
-        info.textContent = `Player ${player === "red" ? 1 : 2} won!`;
-        remove_event();
         return true;
     }
     return false;
-}
-
-// Function to check if the game is a draw
-function is_tie(): boolean {
-    for (let r = 0; r < ROW; r++) {
-        for (let c = 0; c < COL; c++) {
-            if (!board_state[r][c]) return false;
-        }
-    }
-    return true;
-}
-
-// Function to remove event listeners from all cells
-function remove_event(): void {
-    for (let ROW = 0; ROW < 6; ROW++) {
-        for (let COL = 0; COL < 7; COL++) {
-            const cell = document.querySelector(`#row${ROW}_col${COL}`);
-
-            cell?.removeEventListener("click", make_move);
-            cell?.removeEventListener("mouseover", on_mouse_over);
-            cell?.removeEventListener("mouseout", on_mouse_out);
-
-            remove_child(disc_on_hover);
-        }
-    }
-}
-
-// Function to count aligned discs in a horizontal direction
-function count_aligned_disc_horizontal(row: number, col: number, step: number): number {
-    let count = 0;
-
-    while (col >= 0 && col < COL) {
-        let next_col = col + step;
-
-        if (next_col >= 0 && next_col < COL) {
-            if (board_state[row][col] === board_state[row][next_col]) {
-                count += 1;
-            } else {
-                break;
-            }
-        }
-        col = next_col;
-    }
-    return count;
 }
 
 // Function to check for a horizontal win
@@ -137,6 +103,9 @@ function check_vertical(row: number, col: number): boolean {
             if (board_state[next_row][col] === board_state[row][col]) {
                 count += 1;
             }
+            else {
+                break;
+            }
         }
         row = next_row;
     }
@@ -146,6 +115,37 @@ function check_vertical(row: number, col: number): boolean {
     }
 
     return false;
+}
+
+// Function to check for a diagonal win
+function check_diagonals(row: number, col: number): boolean {
+    let count_diag_one = count_aligned_disc_diagonal(row, col, 1, -1) + count_aligned_disc_diagonal(row, col, -1, 1) - 1;
+    let count_diag_two = count_aligned_disc_diagonal(row, col, 1, 1) + count_aligned_disc_diagonal(row, col, -1, -1) - 1;
+
+    if (count_diag_one >= 4 || count_diag_two >= 4) {
+        return true;
+    }
+
+    return false;
+}
+
+// Function to count aligned discs in a horizontal direction
+function count_aligned_disc_horizontal(row: number, col: number, step: number): number {
+    let count = 0;
+
+    while (col >= 0 && col < COL) {
+        let next_col = col + step;
+
+        if (next_col >= 0 && next_col < COL) {
+            if (board_state[row][col] === board_state[row][next_col]) {
+                count += 1;
+            } else {
+                break;
+            }
+        }
+        col = next_col;
+    }
+    return count;
 }
 
 // Function to count aligned discs in a diagonal direction
@@ -170,61 +170,36 @@ function count_aligned_disc_diagonal(row: number, col: number, step1: number, st
     return count;
 }
 
-// Function to check for a diagonal win
-function check_diagonals(row: number, col: number): boolean {
-    let count_diag_one = count_aligned_disc_diagonal(row, col, 1, -1) + count_aligned_disc_diagonal(row, col, -1, 1) - 1;
-    let count_diag_two = count_aligned_disc_diagonal(row, col, 1, 1) + count_aligned_disc_diagonal(row, col, -1, -1) - 1;
-
-    if (count_diag_one >= 4 || count_diag_two >= 4) {
-        return true;
-    }
-
-    return false;
-}
-
-// Function to find the valid row for a given column
-function take_valid_place(col: number): number {
-    for (let r = ROW - 1; r >= 0; r--) {
-        if (board_state[r][col] === 0) {
-            return r;
+// Function to check if the game is a draw
+function is_tie(): boolean {
+    for (let r = 0; r < ROW; r++) {
+        for (let c = 0; c < COL; c++) {
+            if (!board_state[r][c]) return false;
         }
     }
-    return -1;
+    return true;
 }
 
-// Function to create disc hovers for player selection
-function create_discs_hover(): void {
-    for (let c = 0; c < COL; c++) {
-        const disc_created = document.createElement("div");
-        disc_created.classList.add("square", `disc_${c.toString()}`);
-
-        disc_on_hover.appendChild(disc_created);
-    }
-}
-
-// Function to handle mouseover events on cells
+// Function to handle mouseover events on column
 function on_mouse_over(e: Event): void {
-    const cell_selected = e.target as HTMLElement;
-    const col = cell_selected.classList[1][cell_selected.classList[1].length - 1];
+    const col_selected = e.target as HTMLElement;
+    const col = col_selected.classList[1];
+    
+    col_selected.classList.add("active");
 
     const disc_active = document.querySelector(`.disc_${col}`);
     disc_active?.classList.add("visible", `disc_${player}`);
 }
 
-// Function to handle mouseout events on cells
+// Function to handle mouseout events on column
 function on_mouse_out(e: Event): void {
-    const cell_selected = e.target as HTMLElement;
-    const col = cell_selected.classList[1][cell_selected.classList[1].length - 1];
-
+    const col_selected = e.target as HTMLElement;
+    const col = col_selected.classList[1];
+    
+    col_selected.classList.remove("active");
+    
     const disc_active = document.querySelector(`.disc_${col}`);
     disc_active?.classList.remove("visible", `disc_${player}`);
-}
-
-// Function to remove all child elements from a parent element
-function remove_child(parent: HTMLElement): void {
-    while (parent.firstChild) {
-        parent.removeChild(parent.firstChild);
-    }
 }
 
 // Function to reset the game
@@ -239,7 +214,7 @@ function on_reset(): void {
     ]
 
     player = "red";
-    info.textContent = "Red's turn.";
+    info.textContent = "red's turn.";
 
     // Remove all child elements from the game board and disc hovers
     remove_child(board_game);
@@ -249,10 +224,58 @@ function on_reset(): void {
     main();
 }
 
+// Function to remove event listeners from all columns
+function remove_event(): void {
+    const columns = document.getElementsByClassName("column");
+    
+    for (let i = 0; i < columns.length; i++) {
+        const column = columns[i] as HTMLElement;
+
+        column.classList.remove("active");
+        column.removeEventListener("click", make_move);
+        column.removeEventListener("mouseover", on_mouse_over);
+        column.removeEventListener("mouseout", on_mouse_out);
+    }
+
+    remove_child(disc_on_hover);
+}
+
+// Function to remove all child elements from a parent element
+function remove_child(parent: HTMLElement): void {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+// Function to create disc hovers for player selection
+function create_discs_hover(): void {
+    for (let c = 0; c < COL; c++) {
+        const disc_created = document.createElement("div");
+        disc_created.classList.add("square", `disc_${c.toString()}`);
+
+        disc_on_hover.appendChild(disc_created);
+    }
+}
+
+function create_cols_on_hover(): void {
+    for (let col = 0; col < 7; col++) {
+        const col_created = document.createElement("div");
+        col_created.classList.add("column", `${col.toString()}`);
+        col_created.style.left = `${col * 100}px`;
+
+        board_game.appendChild(col_created);
+
+        col_created.addEventListener("mouseover", on_mouse_over);
+        col_created.addEventListener("mouseout", on_mouse_out);
+        col_created.addEventListener("click", make_move);
+    }
+}
+
 // Function to initialize the game
 function main(): void {
     // Create disc hovers for player selection
     create_discs_hover();
+    create_cols_on_hover();
 
     // Create the game board and attach event listeners to cells
     for (let ROW = 0; ROW < 6; ROW++) {
@@ -262,10 +285,6 @@ function main(): void {
             cell_created.id = `row${ROW}_col${COL}`;
 
             board_game.appendChild(cell_created);
-
-            cell_created.addEventListener("click", make_move);
-            cell_created.addEventListener("mouseover", on_mouse_over);
-            cell_created.addEventListener("mouseout", on_mouse_out);
 
             button_reset.addEventListener("click", on_reset);
         }
